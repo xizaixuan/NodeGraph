9900fb6810b39c1a8e8082695dd685fe7c9c20fa
xizaixuan <285963400@qq.com>
2020-02-13 15:29:18 +0800

    add IGraphObject Interface

---
+++ Assets/NodeGraph/Scripts/Data/Interfaces/Graph/IGraphObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ModifierNodeGraph
+{
+    public interface IGraphObject
+    {
+        IGraph graph { get; set; }
+    }
+}
+
