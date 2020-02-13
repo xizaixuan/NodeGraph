7423a01ebc09a595bc8a501c060c39d4afb3e93f
xizaixuan <285963400@qq.com>
2020-02-13 16:25:48 +0800

    add ISerializationCallbackReceiver

--- Assets/NodeGraph/Scripts/Data/Implementation/GraphObject.cs
+++ Assets/NodeGraph/Scripts/Data/Implementation/GraphObject.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 namespace ModifierNodeGraph
 {
-    public class GraphObject : ScriptableObject, IGraphObject
+    public class GraphObject : ScriptableObject, IGraphObject, ISerializationCallbackReceiver
     {
+        [SerializeField]
+        SerializationHelper.JSONSerializedElement m_SerializedGraph;
+
         IGraph m_Graph;
+        IGraph m_DeserializedGraph;
 
         public IGraph graph
         {
@@ -19,6 +23,21 @@ namespace ModifierNodeGraph
                 if (m_Graph != null)
                     m_Graph.owner = this;
             }
+        }
+
+        public void OnBeforeSerialize()
+        {
+            if (graph != null)
+                m_SerializedGraph = SerializationHelper.Serialize(graph);
+        }
+
+        public void OnAfterDeserialize()
+        {
+            var deserializedGraph = SerializationHelper.Deserialize<IGraph>(m_SerializedGraph, null);
+            if (graph == null)
+                graph = deserializedGraph;
+            else
+                m_DeserializedGraph = deserializedGraph;
         }
     }
 }
