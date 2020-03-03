7534c38d979165bfcca03e1445ed62fd1ee279d0
zhangxuan <zhangxuan02@shandagames.com>
2020-03-03 19:28:58 +0800

    modified stringcontrol

--- Assets/NodeGraph/Scripts/Drawing/Controls/StringControl.cs
+++ Assets/NodeGraph/Scripts/Drawing/Controls/StringControl.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Globalization;
+using System.Linq;
 using System.Reflection;
 using UnityEditor;
 using UnityEditor.Experimental.UIElements;
@@ -27,27 +29,62 @@ namespace ModifierNodeGraph
     {
         ModifierNode m_Node;
         PropertyInfo m_PropertyInfo;
+        string m_Value;
 
         public StringControlView(string label, ModifierNode node, PropertyInfo propertyInfo)
         {
             AddStyleSheetPath("Styles/Controls/StringControlView");
             m_Node = node;
             m_PropertyInfo = propertyInfo;
-            //if (!propertyInfo.PropertyType.IsEnum)
-            //    throw new ArgumentException("Property must be an enum.", "propertyInfo");
-            Add(new Label(label ?? ObjectNames.NicifyVariableName(propertyInfo.Name)));
+
+            label = label ?? ObjectNames.NicifyVariableName(propertyInfo.Name);
+            if (!string.IsNullOrEmpty(label))
+                Add(new Label(label));
+
+            m_Value = GetValue();
+
             var textField = new TextField();
-            //enumField.OnValueChanged(OnValueChanged);
+            textField.RegisterCallback<MouseDownEvent>(Repaint);
+            textField.RegisterCallback<MouseMoveEvent>(Repaint);
+            textField.OnValueChanged(evt =>
+            {
+                SetValue(evt.newValue);
+                this.MarkDirtyRepaint();
+            });
+            textField.RegisterCallback<InputEvent>(evt =>
+            {
+                var value = GetValue();
+                SetValue(value);
+                this.MarkDirtyRepaint();
+            });
+            textField.RegisterCallback<KeyDownEvent>(evt =>
+            {
+                if (evt.keyCode == KeyCode.Escape)
+                {
+                    var value = GetValue();
+                    SetValue(value);
+                    evt.StopPropagation();
+                }
+                this.MarkDirtyRepaint();
+            });
             Add(textField);
         }
 
-        void OnValueChanged(ChangeEvent<Enum> evt)
+        string GetValue()
         {
-            var value = (Enum)m_PropertyInfo.GetValue(m_Node, null);
-            if (!evt.newValue.Equals(value))
-            {
-                m_PropertyInfo.SetValue(m_Node, evt.newValue, null);
-            }
+            var value = m_PropertyInfo.GetValue(m_Node, null);
+            return (string)value;
+        }
+
+        void SetValue(string value)
+        {
+            m_PropertyInfo.SetValue(m_Node, value, null);
+        }
+
+        void Repaint<T>(MouseEventBase<T> evt) where T : MouseEventBase<T>, new()
+        {
+            evt.StopPropagation();
+            this.MarkDirtyRepaint();
         }
     }
 }
